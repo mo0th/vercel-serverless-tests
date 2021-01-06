80f24b885ee9cfb1aacd01b35286df5e9849e59b
Soorria Saruva <soorria.ss@gmail.com>
2021-01-06 17:52:47 +1100

    make slightly better

--- api/google-credentials.ts
+++ api/google-credentials.ts
@@ -1,7 +1,8 @@
 import { NowApiHandler } from '@vercel/node'
 
 const handler: NowApiHandler = (req, res) => {
-  res.json({ env: process.env })
+  console.log({ env: process.env })
+  res.json({})
 }
 
 export default handler
